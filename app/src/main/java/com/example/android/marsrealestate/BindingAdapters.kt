2da9750c838dbf7d504b27fd091b79be09805c72
danyramirezg <danyramirezg@gmail.com>
2021-04-26 11:18:43 -0500

    Using glide downloaded and displayed the first image from Mars

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,3 +17,31 @@
 
 package com.example.android.marsrealestate
 
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+@BindingAdapter("imageUrl")
+
+// Function to convert imgUrl to a URI with the https scheme:
+
+fun bindImage(imgView: ImageView, imgUrl: String?){
+    imgUrl?.let{
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+                .load(imgUri)
+        // Indication if the loading fails:
+                .apply(RequestOptions()
+                        .placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image))
+
+                .into(imgView)
+
+
+    }
+
+}
+
